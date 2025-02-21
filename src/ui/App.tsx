04b5fa80d3a1293ead989ade8ca6071088bd04a5
janitha-visna <visnajanitha@gmail.com>
2025-02-21 13:33:48 +0530

    finalizing the ui

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -5,10 +5,10 @@ import { useStatistics } from "./useStatistics";
 import { Chart } from "./Chart";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const staticData = useStaticData();
   const statistics = useStatistics(10);
-  const [activeView,setActiveView] = useState<View>("CPU");
-  const cpuUsage = useMemo(
+  const [activeView, setActiveView] = useState<View>("CPU");
+  const cpuUsages = useMemo(
     () => statistics.map((stat) => stat.cpuUsage),
     [statistics]
   );
@@ -16,61 +16,113 @@ function App() {
     () => statistics.map((stat) => stat.RamUsage),
     [statistics]
   );
-  const storageDatas = useMemo(
+  const storageUsages = useMemo(
     () => statistics.map((stat) => stat.storageData),
     [statistics]
   );
-
   const activeUsages = useMemo(() => {
     switch (activeView) {
       case "CPU":
-        return cpuUsage;
+        return cpuUsages;
       case "RAM":
         return ramUsages;
       case "STORAGE":
-        return storageDatas;
+        return storageUsages;
     }
-  }, [activeView, cpuUsage, ramUsages, storageDatas]);
+  }, [activeView, cpuUsages, ramUsages, storageUsages]);
 
-useEffect(() => {
-  return window.electron.subscribeChangeView((view) => setActiveView(view));
-},[]);
-
-  console.log(statistics);
+  useEffect(() => {
+    return window.electron.subscribeChangeView((view) => setActiveView(view));
+  }, []);
 
   return (
-    <>
-      <header>
-        <button
-          id="close"
-          onClick={() => window.electron.sendFrameAction("CLOSE")}
-        />
-        <button
-          id="minimize"
-          onClick={() => window.electron.sendFrameAction("MINIMIZE")}
-        />
-        <button
-          id="maximize"
-          onClick={() => window.electron.sendFrameAction("MAXIMIZE")}
-        />
-      </header>
-      <div style={{ height: 120 }}>
-        <Chart data={activeUsages} maxDataPoints={10} />
+    <div className="App">
+      <Header />
+      <div className="main">
+        <div>
+          <SelectOption
+            onClick={() => setActiveView("CPU")}
+            title="CPU"
+            view="CPU"
+            subTitle={staticData?.cpuModel ?? ""}
+            data={cpuUsages}
+          />
+          <SelectOption
+            onClick={() => setActiveView("RAM")}
+            title="RAM"
+            view="RAM"
+            subTitle={(staticData?.toatalMemoryGB.toString() ?? "") + " GB"}
+            data={ramUsages}
+          />
+          <SelectOption
+            onClick={() => setActiveView("STORAGE")}
+            title="STORAGE"
+            view="STORAGE"
+            subTitle={(staticData?.totalStorage.toString() ?? "") + " GB"}
+            data={storageUsages}
+          />
+        </div>
+        <div className="mainGrid">
+          <Chart
+            selectedView={activeView}
+            data={activeUsages}
+            maxDataPoints={10}
+          />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
+}
+
+function SelectOption(props: {
+  title: string;
+  view: View;
+  subTitle: string;
+  data: number[];
+  onClick: () => void;
+}) {
+  return (
+    <button className="selectOption" onClick={props.onClick}>
+      <div className="selectOptionTitle">
+        <div>{props.title}</div>
+        <div>{props.subTitle}</div>
+      </div>
+      <div className="selectOptionChart">
+        <Chart selectedView={props.view} data={props.data} maxDataPoints={10} />
+      </div>
+    </button>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <button
+        id="close"
+        onClick={() => window.electron.sendFrameAction("CLOSE")}
+      />
+      <button
+        id="minimize"
+        onClick={() => window.electron.sendFrameAction("MINIMIZE")}
+      />
+      <button
+        id="maximize"
+        onClick={() => window.electron.sendFrameAction("MAXIMIZE")}
+      />
+    </header>
+  );
+}
+
+function useStaticData() {
+  const [staticData, setStaticData] = useState<StaticData | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      setStaticData(await window.electron.getStaticData());
+    })();
+  }, []);
+
+  return staticData;
 }
 
 export default App;
--- src/ui/Chart.tsx
+++ src/ui/Chart.tsx
@@ -4,9 +4,29 @@ import { BaseChart } from "./BaseChart";
 export type ChartProps = {
   data: number[];
   maxDataPoints: number;
+  selectedView: "CPU" | "RAM" | "STORAGE";
+};
+
+export const COLOR_MAP = {
+  CPU: {
+    stroke: "#5DD4EE",
+    fill: "#0A4D5C",
+  },
+  RAM: {
+    stroke: "#E99311",
+    fill: "#5F3C07",
+  },
+  STORAGE: {
+    stroke: "#1ACF4D",
+    fill: "#0B5B22",
+  },
 };
 
 export function Chart(props: ChartProps) {
+    const color = useMemo(
+      () => COLOR_MAP[props.selectedView],
+      [props.selectedView]
+    );
   const preparedData = useMemo(() => {
     // Convert numbers to { value: number } objects
     const points = props.data.map((point) => ({ value: point * 100 }));
@@ -20,5 +40,5 @@ export function Chart(props: ChartProps) {
     return [...points, ...emptyPoints]; // Combine actual and empty data points
   }, [props.data, props.maxDataPoints]); // Dependencies
 
-  return <BaseChart data={preparedData} fill="#4CAF50" stroke="#2E7D32" />;
+  return <BaseChart data={preparedData} fill={color.fill} stroke={color.stroke}/>;
 }
